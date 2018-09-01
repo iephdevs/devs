291c113af0bc5a3e7f532696df91b44a8fa9933c
iephdevs <42347975+iephdevs@users.noreply.github.com>
2018-09-01 13:55:36 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #Menu
 
-echo -e " Welcome to IEPH DEVELOPERS SCRIPT
+echo -e "Welcome to IEPH DEVELOPERS SCRIPT"| lolcat
 
-echo -e "* menu      : Displays a list of available commands"
-echo -e "* usernew   : Creating an SSH Account"
-echo -e "* trial     : Create a Trial Account"
-echo -e "* delete    : Clearing SSH and OpenVPN Account"
-echo -e "* check     : Check User Login"
-echo -e "* member    : Check Member SSH and OpenVPN"
-echo -e "* restart   : Restart Service dropbear, webmin"
+echo -e "* menu | lolcat     : Displays a list of available commands"
+echo -e "* usernew | lolcat  : Creating an SSH Account"
+echo -e "* trial| lolcat     : Create a Trial Account"
+echo -e "* delete| lolcat    : Clearing SSH and OpenVPN Account"
+echo -e "* check| lolcat     : Check User Login"
+echo -e "* member | lolcat   : Check Member SSH and OpenVPN"
+echo -e "* restart | lolcat  : Restart Service dropbear, webmin"
 echo -e "              squid3, OpenVPN and SSH"
-echo -e "* reboot    : reboot VPS"
-echo -e "* speedtest : speedtest VPS"
-echo -e "* info      : System Information"
-echo -e "* about     : Information about auto install script"
-echo -e "* exit      : exit Putty/Connecbot/"
+echo -e "* reboot | lolcat   : reboot VPS"
+echo -e "* speedtest| lolcat : speedtest VPS"
+echo -e "* info  | lolcat    : System Information"
+echo -e "* about | lolcat    : Information about auto install script"
+echo -e "* exit | lolcat     : exit Putty/Connecbot/"
 echo -e "              JuiceSSH"
 echo -e ""
